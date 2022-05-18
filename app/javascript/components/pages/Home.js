500479720898e6f7442fc1fc7dadc7d6b0f9bb5a
dustin-oz <schelldd@gmail.com>
2022-05-18 16:21:48 -0500

    Added index route in controller. Styled home page.

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
   render() {
     return (
       <>
-        <div style={{textAlign: "center"}}>
+        <div className="home-body" style={{textAlign: "center"}}>
           <h3>Welcome to Dustin's Apartment App</h3>
           <h4>Find your next home today!</h4>
         </div>
